6309ee9e7ccaff78a8e66cb58f7fbd8994aec5c8
我爱小杰 <648266313@qq.com>
2020-07-02 23:37:16 +0800

    datatype

---
+++ 基础/2.数据类型.java
@@ -0,0 +1,22 @@
+package com.company.day01;
+/*
+ 数据类型:
+    java 是强类型语言,对于每一种数据都给出了明确的数据类型,不同的数据类型也分配了不同的内存空间,所以数据大小也不一样
+
+    基本数据类型:
+                    数值型:    (内存 取值范围)
+                            整数 byte(1 -128-127) short(2 -32768-32767)  int long
+                            浮点数 float(4) double(8)
+                            字符 char(2)
+                    非数值型:
+                            布尔 boolean(1)
+     引用类型数据:
+                    类  class
+                    接口 interface
+                    数组 []
+*/
+public class DataType {
+    public static  void  main(String[] args){
+
+    }
+}
