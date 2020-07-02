831bf9ff5e9e5a4c6a575df33ae72816c5868c32
我爱小杰 <648266313@qq.com>
2020-07-02 23:18:31 +0800

    init

---
+++ 基础/1.常量.java
@@ -0,0 +1,28 @@
+package com.company.day01;
+/*
+    常量:
+        在程序运行过程中,其值不可以发生改变的值.
+    常量分类:
+        字符串常量:    用双引号括起来的内容.
+        整数常量:      不带小数点的数字.
+        小数常量:      带小数的数字.
+        字符常量:      用单引号括起来的内容.
+        布尔值常量:    布尔值,表示真假  true false
+        空常量:        一个特殊的值,空值  null
+*/
+public class ChangLiang {
+    public static void main(String[] args) {
+        // 字符串常量
+        System.out.println("HELLO");
+        //  整数常量
+        System.out.println(666666);
+        // 小数常量
+        System.out.println(666666.666666);
+        // 字符常量
+        System.out.println('我');
+        // 布尔值常量
+        System.out.println(true);
+        // 空常量(不能直接输出)
+       // System.out.println(null);
+    }
+}
